28754f14cf8822138d08cc823b9a301259862d2e
GiovaniGitHub <giovani.angelo@gmail.com>
2022-03-25 01:35:55 -0300

    feat: Add route and resource of alive endpoint.

---
+++ services/api/project/resourcers/__init__.py
@@ -0,0 +1,10 @@
+from flask_restful import Resource
+
+
+class Alive(Resource):
+	def __init__(self):
+		pass
+	def get(self):
+		return {
+			"status": "alive"
+		}
--- services/api/project/resourcers/routers.py
+++ services/api/project/resourcers/routers.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
-from project.repositories.Hello import Helloworld
+from project.resourcers import Alive
 from project.resourcers.user import LoginResource, UserResource
 
 
@@ -8,7 +8,7 @@ def init_routes(app):
     bp = Blueprint("restapi", __name__, url_prefix="/api")
     api = Api(bp)
 
-    api.add_resource(Helloworld, "/hello")
+    api.add_resource(Alive, "/hello")
     api.add_resource(UserResource, "/users")
     api.add_resource(LoginResource, "/login")
     
