2c2bb74fc2cb5ac2b88b963aacec44866b422ad2
GiovaniGitHub <giovani.angelo@gmail.com>
2022-03-25 01:11:11 -0300

    chore: Add some consts to use in validations.

---
+++ services/api/project/utils/const.py
@@ -0,0 +1,12 @@
+
+from passlib.context import CryptContext
+
+PHOTOS_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
+PWD_CONTEXT = CryptContext(
+    schemes=["pbkdf2_sha256"],
+    default="pbkdf2_sha256",
+    pbkdf2_sha256__default_rounds=30000,
+)
+
+email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
