279f0073a0ae7efe5fefcdb9330474418454efb0
GiovaniGitHub <giovani.angelo@gmail.com>
2022-03-30 00:05:12 -0300

    feat: Add alive router.

--- services/api/project/resourcers/routers.py
+++ services/api/project/resourcers/routers.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
 from flask_restful import Api
-
 from project.resourcers import Alive
 from project.resourcers.album import AlbumResource, FriendAlbumResource
 from project.resourcers.photo import PhotoResource
@@ -11,7 +10,7 @@ def init_routes(app):
     bp = Blueprint("restapi", __name__, url_prefix="/api")
     api = Api(bp)
 
-    api.add_resource(Alive, "/hello")
+    api.add_resource(Alive, "/alive")
     api.add_resource(UserResource, "/users")
     api.add_resource(LoginResource, "/login")
     
