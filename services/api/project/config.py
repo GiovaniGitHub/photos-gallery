600aac57809c730e8f4d6609b8aeb199d67b7ef7
GiovaniGitHub <giovani.angelo@gmail.com>
2022-03-29 23:58:24 -0300

    chore: Change config.py

--- services/api/project/config.py
+++ services/api/project/config.py
@@ -5,8 +5,6 @@ basedir = path.abspath(path.dirname(__file__))
 
 
 class BaseConfig(object):
-    STATIC_FOLDER = f"{environ.get('APP_FOLDER')}/project/static"
-    MEDIA_FOLDER = f"{environ.get('APP_FOLDER')}/project/media"
     SECRET_KEY = environ.get("SECRET_KEY")
     TOKEN_EXPIRES = environ.get("TOKEN_EXPIRES", 120)
 
@@ -14,3 +12,8 @@ class BaseConfig(object):
 class DataBaseConfig(object):
     SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL", "sqlite://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    AWS_ACCESS_KEY_ID = environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_DEFAULT_REGION = environ.get("AWS_DEFAULT_REGION")
+    AWS_S3_LOCATION = environ.get("AWS_S3_LOCATION")
+    AWS_S3_BUCKET_NAME = environ.get("AWS_S3_BUCKET_NAME")
