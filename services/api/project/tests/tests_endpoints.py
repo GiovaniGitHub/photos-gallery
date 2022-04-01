f4f7062820859a17a2eef34ac1fe37463a3c417e
GiovaniGitHub <giovani.angelo@gmail.com>
2022-03-31 23:41:02 -0300

    fix: Improve tests.

--- services/api/project/tests/tests_endpoints.py
+++ services/api/project/tests/tests_endpoints.py
@@ -11,7 +11,8 @@ from project.utils.const import PWD_CONTEXT
 config = dotenv_values(f'{os.getenv("PROJECT_GALLERY")}.env')
 
 fake = Faker()
-header = {"Accept": "*/*","content-type": "application/json"}
+header = {"Accept": "*/*", "content-type": "application/json"}
+
 
 def tests_alive():
     resp = requests.get("http://127.0.0.1:5000/api/alive")
@@ -31,44 +32,67 @@ def tests_create_user():
 
 
 def test_login_user():
-    data_create = {"password": fake.password(8), "email": fake.email(), "name": fake.name()}
+    data_create = {
+        "password": fake.password(8),
+        "email": fake.email(),
+        "name": fake.name(),
+    }
     resp = requests.post(
         "http://127.0.0.1:5000/api/users", headers=header, data=json.dumps(data_create)
     )
     assert resp.status_code == 200
-    
-    data_login = {"password": data_create['password'], "email": data_create["email"]}
+
+    data_login = {"password": data_create["password"], "email": data_create["email"]}
     resp = requests.post(
         "http://127.0.0.1:5000/api/login", headers=header, data=json.dumps(data_login)
     )
     assert resp.status_code == 200
     assert "access_token" in resp.json()
     assert "refresh_token" in resp.json()
-    
+
 
 def test_create_album():
-    data_create = {"password": fake.password(8), "email": fake.email(), "name": fake.name()}
+    data_create = {
+        "password": fake.password(8),
+        "email": fake.email(),
+        "name": fake.name(),
+    }
     resp = requests.post(
         "http://127.0.0.1:5000/api/users", headers=header, data=json.dumps(data_create)
     )
     assert resp.status_code == 200
-    
-    data_login = {"password": data_create['password'], "email": data_create["email"]}
-    resp = requests.post(
+
+    data_login = {"password": data_create["password"], "email": data_create["email"]}
+    resp_login = requests.post(
         "http://127.0.0.1:5000/api/login", headers=header, data=json.dumps(data_login)
     )
     assert resp.status_code == 200
-    
-    assert "access_token" in resp.json()
-    assert "refresh_token" in resp.json()
-    
-    decode = jwt.decode(resp.json()['access_token'], config['SECRET_KEY'], algorithms=['HS256', ])
-    assert decode['sub']['email'] == data_login['email']
-    
+
+    assert "access_token" in resp_login.json()
+    assert "refresh_token" in resp_login.json()
+
+    decode = jwt.decode(
+        resp_login.json()["access_token"],
+        config["SECRET_KEY"],
+        algorithms=[
+            "HS256",
+        ],
+    )
+    assert decode["sub"]["email"] == data_login["email"]
+
     data = {"name": fake.name()}
-    resp = requests.post(
+
+    # TRY WITHOUT AUTHENTICATION AND RECEIVE WRONG (STATUS 401)
+    resp_wrong = requests.post(
         "http://127.0.0.1:5000/api/albums", headers=header, data=json.dumps(data)
     )
 
-    
-    
+    assert resp_wrong.status_code == 401
+
+    # NOW TRY WITH AUTHENTICATION AND RECEIVE SUCCESS (STATUS 201)
+    header["Authorization"] = f"Bearer {resp_login.json()['access_token']}"
+    resp_album = requests.post(
+        "http://127.0.0.1:5000/api/albums", headers=header, data=json.dumps(data)
+    )
+
+    assert resp_album.status_code == 201
